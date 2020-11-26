6b1c18cca7c13915c23622ad664083ddb49bafbf
Daniel Hackl <dan.hackl@gmail.com>
2020-11-26 04:24:20 -0500

    Update(BP): api route + error handler in server

--- server.js
+++ server.js
@@ -1,8 +1,35 @@
 const express = require("express");
 const app = express();
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const books = require("./api/books");
+
 const port = process.env.PORT || 9001;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Book api
+app.use("/api/books", books);
+
+// 404 error and send to error handler
+app.use((req, res, next) => {
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+// Error handler
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: req.app.get("env") === "development" ? err : {},
+  });
+});
+
+// Listen on port 9001
 app.listen(port, () => console.log(`listening on port: ${port}`));
 
-app.get("/", (request, response) => {
-  response.send("We Did it!");
-});
+module.exports = app;
